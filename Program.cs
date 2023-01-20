7a8849ca77505d8ac16d9602652acd889eeb56f6
vimirsi <victormsilva1998@gmail.com>
2023-01-20 00:21:26 -0300

    Criando conexão com banco

--- Program.cs
+++ Program.cs
@@ -3,8 +3,11 @@ using pokedexapi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
 // Add services to the container.
-builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+builder.Services.AddDbContextPool<DataContext>(opt =>
+                opt.UseMySql(mySqlConnection,ServerVersion.AutoDetect(mySqlConnection)));
 builder.Services.AddScoped<DataContext, DataContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,6 +19,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+
+    using(var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+        dbContext.Database.EnsureCreated();
+    }
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
