1eb20c5cf22ea8fb7f99c23928a22e620f633290
vimirsi <victormsilva1998@gmail.com>
2023-02-19 00:51:18 -0300

    Pokedex page em desenvolvimento e subida de script sql para testes

--- PokedexApi.Web/Controllers/HomeController.cs
+++ PokedexApi.Web/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Pokedex()
     {
         return View();
     }
